3302fe6df4de9742060d4b50a7ad96821cb9d931
ziyemao <98372180+ziyemao@users.noreply.github.com>
2022-03-11 11:11:01 +0800

    index.js

--- static/index.js
+++ static/index.js
@@ -348,10 +348,10 @@
             return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
         }
         SubmitResults();
-        if (score <= 49) return '试着好好练一下？';
-        if (score <= 99) return 'TCL';
-        if (score <= 149) return 'TQL';
-        if (score <= 199) return '您';
+        if (score <= 49) return '你这不行啊！';
+        if (score <= 99) return '不错！;
+        if (score <= 149) return '太强了';
+        if (score <= 199) return '芜湖，手速飞起';
         return '人？';
     }
 
